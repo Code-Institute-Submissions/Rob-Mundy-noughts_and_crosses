a1c3bb2031653766ae4836901c1f002cfd4b16a2
Rob-Mundy <robertemundy@googlemail.com>
2022-11-30 16:53:57 +0000

    Add initial function ideas

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def design_board()
+    #creates initial board
+    #refreshes board?
+
+def choose_mark()
+    #asks user to pick an 'x' or 'o'
+    #computer defaults to !user
+
+def which_turn()
+    #determines whether it is the user or computer's turn
+    #use modulo?
+    #improve: randomise first attempt to see who h=goes first
+
+def check_board()
+    #check free spaces
+    #could use separate list?
+
+def check_winner()
+    #check whether there is 3 in a row of either mark (x/o)
+
+def check_full()
+    #are there any spaces left to place a mark?
+    #could the which_turn function incorporate this?
+
+def computer_turn()
+    #random computer guess or apply some logic?
+
+
+

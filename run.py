7339898974f4de9a36fa60af400d7133b8d7457d
Rob-Mundy <robertemundy@googlemail.com>
2022-12-02 11:54:26 +0000

    Add linear design_board function and dictionary of marks (x/o)

--- run.py
+++ run.py
@@ -1,32 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-def design_board()
-    #creates initial board
-    #refreshes board?
+marks = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
 
-def choose_mark()
+def design_board(marks):
+    """
+    creates initial board of 9 spaces using a dictionary/n
+    that corresponds to the numbers on a keypad/telephone
+    """
+    board = (f"{marks[1]}|{marks[2]}|{marks[3]}|{marks[4]}|{marks[5]}|{marks[6]}|{marks[7]}|{marks[8]}|{marks[9]}")
+    print(board)
+
+
+design_board(marks)
+
+#def choose_mark()
     #asks user to pick an 'x' or 'o'
     #computer defaults to !user
 
-def which_turn()
+#def which_turn()
     #determines whether it is the user or computer's turn
     #use modulo?
-    #improve: randomise first attempt to see who h=goes first
+    #improve: randomise first attempt to see who goes first
 
-def check_board()
+#def check_board()
     #check free spaces
     #could use separate list?
 
-def check_winner()
+#def check_winner()
     #check whether there is 3 in a row of either mark (x/o)
 
-def check_full()
+#def check_full()
     #are there any spaces left to place a mark?
     #could the which_turn function incorporate this?
 
-def computer_turn()
-    #random computer guess or apply some logic?
+#def computer_turn()
+    #random computer guess
+    # improve: apply some logic to make it more difficult?
 
 
 

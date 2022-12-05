6c1426ab9b7688fc01f1d103cc7c4e61b639f8c8
Rob-Mundy <robertemundy@googlemail.com>
2022-12-05 14:30:21 +0000

    Add computer_turn random number function

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-#dictionary of marks i.e. spaces where x's and o's can be marked
-#numbered key as key will be visible and values are letters
+import random
+
 
 def design_board(marks):
     """
@@ -19,7 +19,7 @@ def choose_mark():
     user_choice = None
     while user_choice not in {'x', 'o'}:
         user_choice = input('Please select "x" or "o"\n')
-    print(f"You chose {user_choice}")
+    # print(f"You chose {user_choice}")
     return user_choice
 
 
@@ -28,7 +28,7 @@ def computer_mark(user_selection):
     determines computer's choice based on user's input
     """
     computer_choice = 'x' if user_selection == 'o' else 'o'
-    print(f"Computer is {computer_choice}")
+    # print(f"Computer is {computer_choice}")
     return computer_choice
 
 
@@ -44,40 +44,47 @@ def user_turn():
         except ValueError:
             print("Please choose a free space")
             continue
-        if user_input in available_marks:
+        if user_input in available_spaces:
             print(f'You chose {user_input}')
-            available_marks.remove(user_input)
-            #print(available_marks)
-            break
+            available_spaces.remove(user_input)
+            print(available_spaces)
+            # update_marks(marks)
+        break
+
 
 def which_turn():
-    #determines whether it is the user or computer's turn
-    #use modulo?
-    #improve: randomise first attempt to see who goes first
+    """
+    determines whether it is the user's turn or
+    the computer's
+    """
     turn = 0
     if turn % 2 == 0:
         user_turn()
     else:
         computer_turn()
-        turn += 1
+    turn += 1
+    # print(f'Turn is {turn}')
+
+# def update_available()
+    # check free spaces
+    # could use separate list?
+
+# def check_winner()
+    # check whether there is 3 in a row of either mark (x/o)
+
+# def check_full()
+    # are there any spaces left to place a mark?
+    # could the which_turn function incorporate this?
 
 
-#def update_available()
-    #check free spaces
-    #could use separate list?
-
-
-
-#def check_winner()
-    #check whether there is 3 in a row of either mark (x/o)
-
-#def check_full()
-    #are there any spaces left to place a mark?
-    #could the which_turn function incorporate this?
-
-#def computer_turn()
-    #random computer guess
-    # improve: apply some logic to make it more difficult?
+def computer_turn():
+    """
+    selects a random number for the computer
+    from available list of numbers"""
+    computer_input = random.choice(available_spaces)
+    print(f'Computer chose {computer_input}')
+    available_spaces.remove(computer_input)
+    print(available_spaces)
 
 
 def main():
@@ -86,13 +93,13 @@ def main():
     """
     user_selection = choose_mark()
     computer_selection = computer_mark(user_selection)
-    #print(f"Place your mark on the board\nby entering a number between\n1 and 9")
+    # print(f"Place your mark on the board\nby entering a number between\n1 and 9")
     design_board(marks)
     which_turn()
 
 
 
 marks = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
-available_marks = [1,2,3,4,5,6,7,8,9]
+available_spaces = [1,2,3,4,5,6,7,8,9]
 print("let's begin!")
 main()

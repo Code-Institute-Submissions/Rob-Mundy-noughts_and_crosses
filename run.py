70387b263ed9aff8b1dc614682e91e8118b7ab8b
Rob-Mundy <robertemundy@googlemail.com>
2022-12-07 16:23:30 +0000

    Add checkdecAdd check_winner function, removed turn incrementing function and added a Global declar declare_winner variable

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import random
 
 def design_board(marks):
     """
-    creates initial board of 9 spaces using a dictionary/n
+    creates initial board of 9 spaces using a dictionary
     that corresponds to the numbers on a keypad/telephone
     """
     board = (f"{marks[1]}|{marks[2]}|{marks[3]}\n"
@@ -19,7 +19,6 @@ def choose_mark():
     user_choice = None
     while user_choice not in {'x', 'o'}:
         user_choice = input('Please select "x" or "o"\n')
-    # print(f"You chose {user_choice}")
     return user_choice
 
 
@@ -28,89 +27,69 @@ def computer_mark(user_selection):
     determines computer's mark based on user's input
     """
     computer_choice = 'x' if user_selection == 'o' else 'o'
-    # print(f"Computer is {computer_choice}")
     return computer_choice
 
 
 def user_turn():
     """
-    Asks user to choose a space,
-    removes choice from available list,
+    Asks user to choose a space, removes choice from available list,
     places user's mark on the board
     """
     while True:
         try:
             user_input = int(input("Your turn: "))
         except ValueError:
-            print("That isn't available.\nTry again: ")
+            print("That isn't available. Try again: ")
             continue
         if user_input not in available_spaces:
             print("Please choose a FREE space")
             continue
-            # print(f'You chose {user_input}')
         available_spaces.remove(user_input)
-        # print(available_spaces)
-        # update_marks(marks)
         marks[user_input] = user_selection
-        # print(f'You chose {user_input}')
-        # print(available_spaces)
-        design_board(marks)
+        # design_board(marks)
         break
 
 
-def which_turn():
+def check_winner(marks):
     """
-    determines whether it is the user's turn or
-    the computer's
+    check whether there is 3 in a row of either mark (x/o)
     """
-    turn = 0
+    # Check horizontal 3 in a row
+    if (marks[1] == marks[2] == marks[3]) \
+        or (marks[4] == marks[5] == marks[6]) \
+        or (marks[7] == marks[8] == marks[9]):
+        print("We have a winner!")
+        return True
+
+
+def computer_turn():
+    """
+    selects a random number for the computer from available 
+    list of numbers"""
+    print("Computer's turn: ")
+    computer_input = random.choice(available_spaces)
+    available_spaces.remove(computer_input)
+    marks[computer_input] = computer_selection
+    # design_board(marks)
+
+
+marks = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
+available_spaces = [1,2,3,4,5,6,7,8,9]
+user_selection = choose_mark()
+computer_selection = computer_mark(user_selection)
+declare_winner = False 
+turn = 0
+design_board(marks)
+
+while not declare_winner:
+
     while available_spaces:
+
         if turn % 2 == 0:
             user_turn()
         else:
             computer_turn()
         turn += 1
-        # print(f'Turn is {turn}')
-
-
-# def update_marks(user_input):
-    """
-    places user's or computer's mark on board 
-    replacing placeholder number
-    """
-    
-# def check_winner()
-    # check whether there is 3 in a row of either mark (x/o)
-
-# def check_full()
-    # are there any spaces left to place a mark?
-    # could the which_turn function incorporate this?
-
-
-def computer_turn():
-    """
-    selects a random number for the computer
-    from available list of numbers"""
-    print("Computer's turn: ")
-    computer_input = random.choice(available_spaces)
-    available_spaces.remove(computer_input)
-    marks[computer_input] = computer_selection
-    # print(f'Computer chose {computer_input}')
-    # print(available_spaces)
-    design_board(marks)
-
-
-def main():
-    """
-    Run all game functions
-    """
-    design_board(marks)
-    which_turn()
-
-
-marks = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
-available_spaces = [1,2,3,4,5,6,7,8,9]
-print("Let's begin!")
-user_selection = choose_mark()
-computer_selection = computer_mark(user_selection)
-main()
+        design_board(marks)
+        declare_winner = check_winner(marks)
+        break
